d2111e5819b17793e089e047e84dc05112663a34
Joao Victor Degelo <jvtdegelo@gmail.com>
2023-08-06 11:17:13 -0300

    Adiciona mudança para gerar interface quando adiciona novo processo

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -19,48 +19,9 @@
 
 DispatcherConfiguration* config;
 GetterPID* getterPID;
-MemoryHandler* memoryHandler;
+MemoryHandler* memory;
 AbstractDispatcher* dispatcher;
 pthread_mutex_t mutex;
-
-void* listener(void* arg) {
-  while (true) {
-    std::ifstream communicationFileRead("./communication.txt"); 
-    if (communicationFileRead.is_open()) {
-      std::string line;
-      std::getline(communicationFileRead, line);
-      while(!communicationFileRead.eof()){
-        char operation = line[0];
-        int parameter = std::stoi(line.substr(2, line.size()-1));
-        AbstractProcess* newProcess;
-        pthread_mutex_lock(&mutex);
-        if (operation == 'c'){
-          newProcess = new CreateProcess(getterPID->get(), parameter, memoryHandler, dispatcher);
-        }
-        else if (operation == 'k'){
-          newProcess = new KillProcess(getterPID->get(), parameter, memoryHandler, dispatcher);
-        }
-        dispatcher->add(newProcess);
-        pthread_mutex_unlock(&mutex);
-        std::getline(communicationFileRead, line);
-      }
-      communicationFileRead.close();
-
-      std::ofstream communicationFileWrite("./communication.txt");
-      if (!communicationFileWrite) {
-        std::cerr << "Error opening the communication file for writing." << std::endl;
-      }
-      communicationFileWrite.close();
-
-    } else {
-      std::cout << "Unable to open the communication file" << std::endl;
-    }
-        
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
-
-  return nullptr;
-}
 
 std::string fill_end_line(std::string str, int maxSize){
   if(maxSize> str.size())
@@ -69,7 +30,10 @@ std::string fill_end_line(std::string str, int maxSize){
 
 }
 
-void show_interface(AbstractProcess* process, AbstractDispatcher* dispatcher, MemoryHandler* memory){
+void show_interface(){
+  pthread_mutex_lock(&mutex);
+  AbstractProcess* process = dispatcher->getCurrentProcess();
+  pthread_mutex_unlock(&mutex);
   std::vector<std::string> emptyVector;
   std::vector<std::string> tcbProcess = process == nullptr? emptyVector: process->getTCB();
   std::vector<std::string> statusProcess = process == nullptr? emptyVector: process->getStatus();
@@ -91,6 +55,51 @@ void show_interface(AbstractProcess* process, AbstractDispatcher* dispatcher, Me
   std::cout<< std::endl;
 }
 
+
+void* listener(void* arg) {
+  while (true) {
+    bool created = false;
+    std::ifstream communicationFileRead("./communication.txt"); 
+    if (communicationFileRead.is_open()) {
+      std::string line;
+      std::getline(communicationFileRead, line);
+      while(!communicationFileRead.eof()){
+        char operation = line[0];
+        int parameter = std::stoi(line.substr(2, line.size()-1));
+        AbstractProcess* newProcess;
+        pthread_mutex_lock(&mutex);
+        if (operation == 'c'){
+          newProcess = new CreateProcess(getterPID->get(), parameter, memory, dispatcher);
+        }
+        else if (operation == 'k'){
+          newProcess = new KillProcess(getterPID->get(), parameter, memory, dispatcher);
+        }
+        dispatcher->add(newProcess);
+        pthread_mutex_unlock(&mutex);
+        created=true;
+        std::getline(communicationFileRead, line);
+      }
+      communicationFileRead.close();
+
+      std::ofstream communicationFileWrite("./communication.txt");
+      if (!communicationFileWrite) {
+        std::cerr << "Error opening the communication file for writing." << std::endl;
+      }
+      communicationFileWrite.close();
+
+    } else {
+      std::cout << "Unable to open the communication file" << std::endl;
+    }
+    
+    if(created)
+      show_interface();
+         
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+  return nullptr;
+}
+
+
 AbstractDispatcher* get_dispatcher(DispatcherConfiguration* config){
   if (config->schedulingAlgorithm == 0)
     return new FIFODispatcher();
@@ -105,7 +114,7 @@ int main(){
 
   config = new DispatcherConfiguration();
   getterPID = new GetterPID();
-  memoryHandler = new MemoryHandler();
+  memory = new MemoryHandler();
   dispatcher = get_dispatcher(config);
   int thread_num = 1;
   int result = pthread_create(&thread, nullptr, listener, nullptr);
@@ -114,10 +123,7 @@ int main(){
     return 1;
   }
   while(true){
-		pthread_mutex_lock(&mutex);
-    AbstractProcess* p = dispatcher->getCurrentProcess();
-    pthread_mutex_unlock(&mutex);
-    show_interface(p, dispatcher, memoryHandler);
+    show_interface();
     std::string line;
     getline(std::cin, line);
     pthread_mutex_lock(&mutex);
